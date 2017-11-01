d43be030b1f8e7ddc0c469e8b55422158602ceac
Christian Zenaty <cazenaty@yahoo.com>
2017-11-01 16:49:05 -0700

    complete and cleaned up styling for links

--- src/Components/Links/AddLink.js
+++ src/Components/Links/AddLink.js
@@ -1,29 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
+import classNames from "classnames";
 
-const AddLink = props => {
-  return (
-    <div className="add-link-container">
-      <form onSubmit={props.handleSubmit}>
-        <input
-          className="add-link-input"
-          name="name"
-          type="text"
-          placeholder="Name"
-          value={props.name}
-          onChange={props.handleChange}
-        />
-        <input
-          className="add-link-input"
-          name="url"
-          type="text"
-          placeholder="URL"
-          value={props.url}
-          onChange={props.handleChange}
-        />
-        <input className="add-link-input-button" type="submit" value="+" />
-      </form>
-    </div>
-  );
-};
+class AddLink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      addLink: false
+    };
+  }
+
+  addLinkToggle = () => {
+    if (this.state.addLink === false) {
+      this.setState({ addLink: true });
+    } else {
+      this.setState({ addLink: false });
+    }
+  };
+
+  render() {
+    let linkFormToggle = classNames({
+      "add-link-form": true,
+      hidden: !this.state.addLink
+    });
+    let linkLabelToggle = classNames({
+      "add-link-click-label": true,
+      hidden: this.state.addLink
+    });
+    return (
+      <div className="add-link-container">
+        <div className={linkLabelToggle} onClick={this.addLinkToggle}>
+          <span>New Link</span>
+        </div>
+        <div className={linkFormToggle}>
+          <form onSubmit={this.props.handleSubmit}>
+            <input
+              className="add-link-input"
+              name="name"
+              type="text"
+              placeholder="Name"
+              value={this.props.name}
+              onChange={this.props.handleChange}
+            />
+            <input
+              className="add-link-input"
+              name="url"
+              type="text"
+              placeholder="URL"
+              value={this.props.url}
+              onChange={this.props.handleChange}
+            />
+            <input className="add-link-input-button" type="submit" value="+" />
+          </form>
+          <span className="add-link-close-button" onClick={this.addLinkToggle}>
+            X
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default AddLink;
