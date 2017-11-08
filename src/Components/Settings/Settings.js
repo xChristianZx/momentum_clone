96d2e787f2f176737683d15c83567faab0d7cf36
Christian Zenaty <cazenaty@yahoo.com>
2017-11-07 18:31:55 -0800

    updated settings

--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -4,8 +4,12 @@ import SetIcon from "react-icons/lib/md/settings";
 
 const Settings = () => {
   return (
-    <div className="settings-icon">
-      <SetIcon />
+    <div className="settings-container">
+      <SetIcon className="settings-icon" />
+      <div className="location-container">
+        <p className="location-title">Antelope Canyon, Arizona</p>
+        <p className="location-photographer">Photo by Christian Zenaty</p>
+      </div>
     </div>
   );
 };
