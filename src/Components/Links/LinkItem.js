f781c759491111ea62cb19b62a0ef4d537cb9884
Christian Zenaty <cazenaty@yahoo.com>
2017-11-09 13:22:34 -0800

    added https check for Links

--- src/Components/Links/LinkItem.js
+++ src/Components/Links/LinkItem.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 const LinkItem = props => {
+  
+  const httpsCheck = (link) => {
+    if (!link.includes("http") || !link.includes("https")) {
+      return `https://${link}`;
+    }
+    return link;
+  };
+
   const linkList = props.links.map(link => (
     <li className="links-list-item" key={link.id}>
-      <a href={link.url} target="_blank">
+      <a href={httpsCheck(link.url)} target="_blank">
         <span>{link.name}</span>
       </a>
-      <button className="list-item-delete-button" type="submit" onClick={props.deleteItem.bind(this, link.id)}>
+      <button
+        className="list-item-delete-button"
+        type="submit"
+        onClick={props.deleteItem.bind(this, link.id)}
+      >
         x
       </button>
     </li>
