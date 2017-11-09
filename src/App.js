bea4c6140d48ec9b5b1fb7eb6980792f0160058b
Christian Zenaty <cazenaty@yahoo.com>
2017-11-09 13:27:21 -0800

    focus ext fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Search from "./Components/Search/Search";
 import Links from "./Components/Links/Links";
 import ToDO from "./Components/ToDo/ToDo";
 import Greeting from "./Components/Greeting/Greeting";
-import Focus from "./Components/Focus/Focus.jsx";
+import Focus from "./Components/Focus/Focus";
 import Quotes from "./Components/Quotes/Quotes";
 import Settings from "./Components/Settings/Settings";
 import Weather from "./Components/Weather/Weather";
